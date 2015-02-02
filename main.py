cee0625c47bd620be7c4a5a655515b3042771397
flightlevel <flightlevel@users.noreply.github.com>
2015-02-02 21:21:00 +1100

    Incuded age of release data

--- main.py
+++ main.py
@@ -10,33 +10,33 @@ import time
 
 log = CPLog(__name__)
 
-class TorrentDetails(object):
 
-    def __init__(self, seeders, leechers, permalink, downlink, torrentID, torrentName, filesize, freeleech, qualityEncode, torrentScore, datetorrentadded):
+class TorrentDetails(object):
+    def __init__(self, seeders, leechers, permalink, downlink, torrentid, torrentname, filesize, freeleech,
+                 qualityencode, torrentscore, datetorrentadded, ageindays):
         self.seeders = seeders
         self.leechers = leechers
         self.permalink = permalink
         self.downlink = downlink
-        self.torrentID = torrentID
-        self.torrentName = torrentName
+        self.torrentid = torrentid
+        self.torrentname = torrentname
         self.filesize = filesize
         self.freeleech = freeleech
-        self.qualityEncode = qualityEncode
-        self.torrentScore = torrentScore
+        self.qualityencode = qualityencode
+        self.torrentscore = torrentscore
         self.datetorrentadded = datetorrentadded
-
+        self.ageindays = ageindays
 
 
 class TehConnection(TorrentProvider, MovieProvider):
-
     urls = {
-            'baseurl': 'https://tehconnection.eu',
-            'login': 'https://tehconnection.eu/login.php',
-            'login_check': 'https://tehconnection.eu/index.php',
-            'search': 'https://tehconnection.eu/torrents.php?searchstr=%s&action=basic',
+        'baseurl': 'https://tehconnection.eu',
+        'login': 'https://tehconnection.eu/login.php',
+        'login_check': 'https://tehconnection.eu/index.php',
+        'search': 'https://tehconnection.eu/torrents.php?searchstr=%s&action=basic',
     }
 
-    http_time_between_calls = 2 #seconds
+    http_time_between_calls = 2  # seconds
 
     def _searchOnTitle(self, title, movie, quality, results):
 
@@ -48,121 +48,124 @@ class TehConnection(TorrentProvider, MovieProvider):
             onlyfreeleech = False
 
         if self.conf('only_verified'):
-            onlyVerified = True
+            onlyverified = True
         else:
-            onlyVerified = False
+            onlyverified = False
 
-
-        if not '/logout.php' in self.urlopen(self.urls['login'], data = self.getLoginParams()).lower():
+        if not '/logout.php' in self.urlopen(self.urls['login'], data=self.getLoginParams()).lower():
             log.info('problems logging into tehconnection.eu')
             return []
 
         data = self.getHTMLData(self.urls['search'] % tryUrlencode(getIdentifier(movie)))
         if data:
             try:
-                resultsTable = BeautifulSoup(data).find('table', attrs = {'id' : 'browse_torrent_table'})
-                if resultsTable is None:
+                resultstable = BeautifulSoup(data).find('table', attrs={'id': 'browse_torrent_table'})
+                if resultstable is None:
                     log.info('movie not found on TehConnection')
                     return []
 
-                pagelinkdata = resultsTable.find("a", { "title" : "View Torrent" })
+                pagelinkdata = resultstable.find("a", {"title": "View Torrent"})
                 torrentpage = (pagelinkdata.attrs['href']).strip()
-                indivTorrData = self.getHTMLData(self.urls['baseurl'] + (torrentpage))
+                indivtorrdata = self.getHTMLData(self.urls['baseurl'] + torrentpage)
 
-                soup = BeautifulSoup(indivTorrData)
-                items = soup.findAll("div", { "class" : "torrent_widget box pad" })
+                soup = BeautifulSoup(indivtorrdata)
+                items = soup.findAll("div", {"class": "torrent_widget box pad"})
                 for item in items:
 
-                    torrentData = TorrentDetails(0, 0, '', '', 0, '', '', False, False, 0, 0)
+                    torrentdata = TorrentDetails(0, 0, '', '', 0, '', '', False, False, 0, 0, 0)
 
+                    detailstats = item.find("div", {"class": "details_stats"})
 
-                    detailstats = item.find("div", { "class" : "details_stats" })
+                    # seeders
+                    seed = detailstats.find("img", {"title": "Seeders"}).parent
+                    torrentdata.seeders = (seed.text.strip())
 
-                    #seeders
-                    seed = detailstats.find("img", { "title" : "Seeders" }).parent
-                    torrentData.seeders = ((seed.text).strip())
-
-                    #leechers
-                    leech = detailstats.find("img", { "title" : "Leechers" }).parent
-                    torrentData.leechers = ((leech.text).strip())
+                    # leechers
+                    leech = detailstats.find("img", {"title": "Leechers"}).parent
+                    torrentdata.leechers = (leech.text.strip())
 
                     #permalink
-                    perma = detailstats.find("a", { "title" : "Permalink" })
-                    torrentData.permalink = self.urls['baseurl'] + perma.attrs['href']
+                    perma = detailstats.find("a", {"title": "Permalink"})
+                    torrentdata.permalink = self.urls['baseurl'] + perma.attrs['href']
 
                     #download link
-                    downlo = detailstats.find("a", { "title" : "Download" })
-                    torrentData.downlink = self.urls['baseurl'] + downlo.attrs['href']
+                    downlo = detailstats.find("a", {"title": "Download"})
+                    torrentdata.downlink = self.urls['baseurl'] + downlo.attrs['href']
 
                     #Torrent ID
-                    m = re.search(r'\d+$', torrentData.permalink)
-                    torrentData.torrentID = (int(m.group()) if m else None)
+                    m = re.search(r'\d+$', torrentdata.permalink)
+                    torrentdata.torrentid = (int(m.group()) if m else None)
 
-                    #TorrentName
-                    namedata = item.find("div", { "id" : "desc_%s" % torrentData.torrentID })
-                    torrentData.torrentName = ((namedata.text).splitlines()[1]).strip()
+                    #torrentname
+                    namedata = item.find("div", {"id": "desc_%s" % torrentdata.torrentid})
+                    torrentdata.torrentname = (namedata.text.splitlines()[1]).strip()
 
                     #FileSize
-                    sizedata = item.find("div", { "class" : "details_title" })
-                    sizefile = ((sizedata.text).splitlines()[3]).replace("(","").replace(")","").strip()
-                    torrentData.filesize = sizefile
+                    sizedata = item.find("div", {"class": "details_title"})
+                    sizefile = (sizedata.text.splitlines()[3]).replace("(", "").replace(")", "").strip()
+                    torrentdata.filesize = sizefile
 
                     #FreeLeech
-                    freeleechdata = item.find("span", { "class" : "freeleech" })
+                    freeleechdata = item.find("span", {"class": "freeleech"})
                     if freeleechdata is None:
-                        torrentData.freeleech = False
+                        torrentdata.freeleech = False
                     else:
-                        torrentData.freeleech = True
+                        torrentdata.freeleech = True
 
                     #QualityEncode
-                    qualityenc = detailstats.find("img", { "class" : "approved" })
+                    qualityenc = detailstats.find("img", {"class": "approved"})
                     if qualityenc is None:
-                        torrentData.qualityEncode = False
+                        torrentdata.qualityencode = False
                     else:
-                        torrentData.torrentName += " HQ"
-                        torrentData.qualityEncode = True
+                        torrentdata.torrentname += " HQ"
+                        torrentdata.qualityencode = True
 
                     #TorrentScore
                     torrscore = 0
-                    if torrentData.qualityEncode == True:
+                    if torrentdata.qualityencode is True:
                         torrscore += self.conf('extrascore_qualityencode')
-                    if torrentData.freeleech == True:
+                    if torrentdata.freeleech is True:
                         torrscore += self.conf('extrascore_freelech')
-                    torrentData.torrentScore = torrscore
+                    torrentdata.torrentscore = torrscore
 
                     #datetorrentadded
                     try:
-                        addeddata = item.find("div", { "class" : "details" })
-                        addedparagraphdata = addeddata.find("p", { "style" : "float: left" })
+                        addeddata = item.find("div", {"class": "details"})
+                        addedparagraphdata = addeddata.find("p", {"style": "float: left"})
                         dateaddedstr = (addedparagraphdata.find('span')['title']).strip()
                         addeddatetuple = time.strptime(dateaddedstr, '%b %d %Y, %H:%M')
-                        torrentData.datetorrentadded = int(time.mktime(addeddatetuple))
+                        torrentdata.datetorrentadded = int(time.mktime(addeddatetuple))
                     except:
                         log.error('Unable to convert datetime from %s: %s', (self.getName(), traceback.format_exc()))
-                        torrentData.datetorrentadded = 0
+                        torrentdata.datetorrentadded = 0
+
+                    #ageindays
+                    torrentdata.ageindays = int((time.time() - torrentdata.datetorrentadded) / 24 / 60 / 60)
 
                     #Test if the Freelech or Verified boxes have been checked & add depending
-                    if (onlyfreeleech == False) or (onlyfreeleech == True and torrentData.freeleech == True):
-                        #Only Freelech is switched off OR only Freelech is ON and the torrent is a freeleech, so safe to add to results
-                        if (onlyVerified == False) or (onlyVerified == True and torrentData.qualityEncode == True):
-                            #Only Verified is switched off OR only Verified is ON and the torrent is verified, so safe to add to results
-                            torrentlist.append(torrentData)
-
+                    if (onlyfreeleech is False) or (onlyfreeleech is True and torrentdata.freeleech is True):
+                        #Only Freelech is switched off OR only Freelech is ON and the torrent is a freeleech,
+                        # so safe to add to results
+                        if (onlyverified is False) or (onlyverified is True and torrentdata.qualityencode is True):
+                            #Only Verified is switched off OR only Verified is ON and the torrent is verified,
+                            # so safe to add to results
+                            torrentlist.append(torrentdata)
 
                 log.info('Number of torrents found from TehConnection = ' + str(len(torrentlist)))
 
-                for torrentFind in torrentlist:
-                    log.info('TehConnection found ' + torrentFind.torrentName)
+                for torrentfind in torrentlist:
+                    log.info('TehConnection found ' + torrentfind.torrentname)
                     results.append({
-                        'leechers': torrentFind.leechers,
-                        'seeders': torrentFind.seeders,
-                        'name': torrentFind.torrentName,
-                        'url': torrentFind.downlink,
-                        'detail_url': torrentFind.permalink,
-                        'id': torrentFind.torrentID,
-                        'size': self.parseSize(torrentFind.filesize),
-                        'score': torrentFind.torrentScore,
-                        'date': torrentFind.datetorrentadded
+                        'leechers': torrentfind.leechers,
+                        'seeders': torrentfind.seeders,
+                        'name': torrentfind.torrentname,
+                        'url': torrentfind.downlink,
+                        'detail_url': torrentfind.permalink,
+                        'id': torrentfind.torrentid,
+                        'size': self.parseSize(torrentfind.filesize),
+                        'score': torrentfind.torrentscore,
+                        'date': torrentfind.datetorrentadded,
+                        'age': torrentfind.ageindays
                     })
 
             except:
